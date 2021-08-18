bdde3dbee3f418e2e077eecedb4469c8386fda20
Ryan J <76914994+RyanJ-NBCC@users.noreply.github.com>
2021-08-18 16:55:07 -0300

    Updated Styling

--- js/site.js
+++ js/site.js
@@ -1,3 +1,39 @@
-function helloWorld(){
-    alert("Hello World!");                                
+/**
+ * Get input string from user
+ * Controller Function
+ */
+function getValue(){
+    document.getElementById('alert').classList.add('invisible');
+
+    let userString = document.getElementById('userString').value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+/**
+ * Reverse the string
+ * Logic Function
+ */
+function reverseString(string){
+    let revString = [];
+      
+    for (let index = string.length-1; index >= 0; index--) {
+        revString += string[index];
+    }
+
+    return revString;
+}
+
+/**
+ * Display message with reversed string
+ * View Function
+ */
+function displayString(string){
+    //write message to page 
+    document.getElementById('message').innerHTML = `Your string reversed is ${string}`;
+
+    //show the alert box
+    document.getElementById('alert').classList.remove('invisible');
 }
